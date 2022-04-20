b17ae8c7b912cd5ef3a8aed73bb99f21801b3325
ASUS <ASUS@LAPTOP-919P8GVA>
2022-04-20 10:33:33 +0530

    commit

--- JAVAcore/src/UNIT_01/P2_Task02_KeyWordsInJava.java
+++ JAVAcore/src/UNIT_01/P2_Task02_KeyWordsInJava.java
@@ -1,37 +1,66 @@
 package UNIT_01;
 
-public class P2_Task02_KeywordsInJava{
+/*
+ *- There are 50 keywords in java
+ *-words with reserved meaning to compiler
+ *48 in use, 2 keywords goto* and const* are for future use
+ *They do not have meaning defined yet
+ */
+
+/*
+ * -Keywords cannot be used as identifiers in java
+ * -variable name
+ * -function name
+ * -class
+ * -object
+ */
+
+
+
+//Write a java program to demonstrate the errors occurred when trying to use 
+//keywords as identifiers
+
+public class P2_Task02_KeyWordsInJava {
 
 	public static void main(String[] args) {
-		int double = 12;
+		
+		//variable name
+		int double =12;  
+		//Syntax error on token "double", invalid VariableDeclaratorId
 		
 		//function name
-		int new() {
-			System.out.println("\nhello world\n");
-			 return 0;
+		int new()
+		{
+			//sysout[ctrl+space]
+			System.out.println("hello world");
+			return 0;
 		}
+		//Syntax error on token "double", invalid VariableDeclaratorId
 		
-		//class name[given below]
 		
-		//object
+		//object name
+		ABC class = new ABC();
 		//object name can never be any keyword in java
-		
-		ABC int =new ABC();
 
 	}
 
 }
-//class name can never be any keywords in java
-class int{
-	void display() {
-		System.out.println("\nanother class\n");
-	}
-}
 
-class ABC{
-	void display() {
-		System.out.println("\nanother class\n");
+
+//class name
+//class name can never be any keyword in java
+class int
+{
+	void display()
+	{
+		System.out.println("Another class");
 	}
-}	
-	
+}//Syntax error on token "double", invalid VariableDeclaratorId
+
+class ABC
+{
+	void display()
+	{
+		System.out.println("Another class");
+	}
 }
